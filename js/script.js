dbb204200e31a6249ba5dbe1a3325c83f591bf13
MD. SHA NEWAZ <70477038+mdshanewaz@users.noreply.github.com>
2023-04-17 22:12:27 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,4 +24,56 @@
 // console.log(c)
 // console.log(d)
 
+// let a, b;
 
+// a = 5;
+// b = 4;
+
+// var c = (a >= b)
+
+// console.log((a>b) || (b<=a));
+
+// var d = (1<4) ? "Hello":"world";
+// console.log(d);
+
+// var a = 12;
+// var c = ++a;
+// var b = a++;  
+
+// console.log(c);
+// console.log(b);
+
+// var a = prompt("Enter temperture");
+// var b = (9/5*a) + 32;
+
+// console.log(a);
+// console.log(b);
+
+
+//There can be two things after the "."
+//One is attribute
+//Another is method, where we have to give () brackets
+
+x = "Hello World!";
+console.log(x);
+
+y = x.length //This is attribute
+console.log(y);
+
+z = x.toUpperCase() //This is method
+console.log(z);
+
+a = x.slice(1, 4);
+console.log(a)
+
+b = x.slice(-5, -1);
+console.log(b);
+
+c = x.substr(3, 3);
+console.log(c);
+
+d = x.replace("ll", "kk");
+console.log(d);
+
+e = c.concat(x, "ABC", "123");
+console.log(e);
